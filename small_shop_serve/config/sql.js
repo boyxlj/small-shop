45d2a514b082cf67be3f76b2640f6d6155ff949d
boyxlj <x709500@163.com>
2023-09-05 21:52:53 +0800

    fix:后台代理导致项目无法运行

--- small_shop_serve/config/sql.js
+++ small_shop_serve/config/sql.js
@@ -1,14 +1,9 @@
 const mysqlConfig = {
-  host:"60.205.226.237",
+  host:"localhost",
   database:"wei_shop",
-  user:"wei_shop",
-  password:"wasd123456",
+  user:"root",
+  password:"",
   port:"3306"
-  // host:"localhost",
-  // database:"wei_shop",
-  // user:"root",
-  // password:"",
-  // port:"3306"
 }
 
 module.exports = mysqlConfig
